de87b042c50623da280590cf0d8e81f06279b215
unknown <vivekbindal1209@gmail.com>
2020-08-29 22:57:21 +0530

    Intro Page develope -Prompt()

---
+++ 1_introExercise/js/app.js
@@ -0,0 +1,6 @@
+var firstName =prompt("what is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age =prompt("What is your age?");
+var fullName=firstName + " "+lastName;
+console.log("Your Full Name is " + fullName);
+console.log("Your age is "+ age +" Years.");
